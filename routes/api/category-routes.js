df93f17329603849adca288438bab8991dfaca4a
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-08-17 20:26:49 -0400

    corrected sign mysql sign in information in.env

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,6 +32,7 @@ router.get("/:id", (req, res) => {
     include: [
       {
         model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
       },
     ],
   })
